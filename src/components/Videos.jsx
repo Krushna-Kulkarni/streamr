ca1403d8ca323bc6d20e7ebfa1c65adcf6ed5b18
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-07-26 05:17:01 +0530

    feat: added videoDetail page to play video

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,12 +1,12 @@
 import { Stack,Box } from '@mui/material'
 import {VideoCard,ChannelCard} from './'
 
-const Videos = ({videos}) => {
-   
+const Videos = ({videos, direction}) => {
+   if(!videos?.length) return 'Loading...'
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="flex-start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
         {
-            videos.filter((content) => content?.id.hasOwnProperty('channelId')  || content?.id.hasOwnProperty('videoId' )).map((item, i)=>(
+            videos?.filter((content) => content?.id.hasOwnProperty('channelId')  || content?.id.hasOwnProperty('videoId' )).map((item, i)=>(
                 <Box key={i}>
                     {
                         item.id.videoId && <VideoCard video={item} />
